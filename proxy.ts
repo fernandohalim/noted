f3f864aefa328532339ea66ca2c3d7961876feeb
Fernando Halim <fernandohalim26@gmail.com>
2026-05-12 16:03:39 +0700

    edit proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,13 +1,13 @@
 import { updateSession } from "@/lib/supabase/middleware";
 import type { NextRequest } from "next/server";
 
-// before: export async function middleware(request: NextRequest)
 export async function proxy(request: NextRequest) {
   return await updateSession(request);
 }
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    // added js and json to the ignored extensions
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|js|json)$).*)",
   ],
 };
